5d4e6608b344faeebb18f13453aaec09ff2d7183
Maksim Kozhukh <mkozhukh@ya.ru>
2019-12-09 22:42:30 +0000

    [update] measure execution time

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"advent/t09"
+	"fmt"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	t09.Star1("t09/data.txt")
+	elapsed := time.Since(start)
+	fmt.Printf("took %s\n\n", elapsed)
+
+	start = time.Now()
 	t09.Star2("t09/data.txt")
+	elapsed = time.Since(start)
+	fmt.Printf("took %s\n\n", elapsed)
 }
